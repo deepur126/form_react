b9849632e172d24bb5e291f78ba47e812735971f
Deepak Rao <57888693+deepur126@users.noreply.github.com>
2020-09-09 11:58:09 +0530

    first commit

--- src/ViewCustomers.js
+++ src/ViewCustomers.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-function ViewCustomers(){
-    return(
-        <div>
-        <h6>This is Customer Details</h6>
+class ViewCustomers extends React.Component {
+    render() {
+        return (
+            <div>
+                <h6>This is Customer Details</h6>
 
-        <div>
-           <ul>
-                <li>
-                    Customer ID: {customElements.customerid}<br/>
-                    Customer Name: {customElements.customername}<br/>
+                <div>
+                    <ul>
+                        <li>
+                            Customer ID: {customElements.customerid}<br />
+                    Customer Name: {customElements.customername}<br />
                     Country: {customElements.country}
-                </li>
-            </ul>
-        </div>
-        </div>
-    );
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ViewCustomers;
